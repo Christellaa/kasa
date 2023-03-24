9d8799a318e37152cd77a18b7f93bb073050982d
Christella De Sousa <desousa.christella@proton.me>
2023-03-24 14:30:49 +0100

    Create banner file

---
+++ src/components/Banner.js
@@ -0,0 +1,12 @@
+import '../styles/Banner.css';
+import logo from '../assets/logo.png';
+
+function Banner() {
+  return (
+    <div className='banner'>
+      <img src={logo} alt="Kasa" className='logo'/>
+    </div>
+  )
+}
+
+export default Banner;
