05b3c3ca87c255f595363922fa1b045d0f306f32
Christella De Sousa <desousa.christella@proton.me>
2023-03-24 15:27:42 +0100

    Modify index file to add home instead of banner

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './styles/index.css';
-import Banner from './components/Banner';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './styles/index.css'
+import Home from './pages/Home.jsx'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <Banner />
+    <Home />
   </React.StrictMode>
-);
+)
