f23f871b6e0105fee073aafcb50e696083ceb1b8
Christella De Sousa <desousa.christella@proton.me>
2023-03-25 15:40:57 +0100

    Add footer

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,15 @@
 import Header from '../components/Header'
 import AboutBanner from '../components/AboutBanner'
+import Footer from '../components/Footer'
+
 function About() {
   return (
     <div className="font-primary">
       <div className="container">
         <Header />
         <AboutBanner />
+      </div>
+      <Footer />
     </div>
   )
 }
