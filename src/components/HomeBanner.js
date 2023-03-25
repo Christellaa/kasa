c0147493a91d901ec187cff83af44d799f100696
Christella De Sousa <desousa.christella@proton.me>
2023-03-25 14:52:27 +0100

    Create home banner

---
+++ src/components/HomeBanner.js
@@ -0,0 +1,12 @@
+import homeBanner from '../assets/homeBanner.png'
+
+function HomeBanner() {
+  return (
+    <div className="banner">
+      <img src={homeBanner} alt="Bannière" className="banner__img" />
+      <h1 className="font-primary">Chez vous, partout et ailleurs</h1>
+    </div>
+  )
+}
+
+export default HomeBanner
