b285ab8fab8c22816dff89d27511a3708b3f6cad
Christella De Sousa <desousa.christella@proton.me>
2023-03-25 15:40:29 +0100

    Create about banner

---
+++ src/components/AboutBanner.js
@@ -0,0 +1,11 @@
+import aboutBanner from '../assets/aboutBanner.png'
+
+function AboutBanner() {
+  return (
+    <div className="banner">
+      <img src={aboutBanner} alt="Bannière" className="banner__img" />
+    </div>
+  )
+}
+
+export default AboutBanner
