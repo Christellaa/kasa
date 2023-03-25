084735fbb866f502dde173213c8b4ca519f34e25
Christella De Sousa <desousa.christella@proton.me>
2023-03-25 11:35:35 +0100

    Fix react router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './styles/index.css'
 import Home from './pages/Home.jsx'
+import Housing from './pages/Housing.jsx'
+import About from './pages/About.jsx'
+import Error from './pages/Error.jsx'
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
-  <React.StrictMode>
-    <Home />
-  </React.StrictMode>
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+    errorElement: <Error />,
+  },
+  {
+    path: '/housing',
+    element: <Housing />,
+  },
+  {
+    path: '/about',
+    element: <About />,
+  },
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
 )
