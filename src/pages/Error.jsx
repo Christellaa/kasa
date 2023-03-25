8db328cb9fef8a3a7bab88c693bd6eea0f50a49f
Christella De Sousa <desousa.christella@proton.me>
2023-03-25 16:02:15 +0100

    Add content

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -5,6 +5,15 @@ function Error() {
     <div>
       <div className="error font-primary container">
         <Header />
+        <div className="error__content">
+          <h1>404</h1>
+          <p className="size-tertiary">
+            Oups! La page que vous demandez n'existe pas.
+          </p>
+          <Link to="/" className="links size-primary">
+            Retourner sur la page d’accueil
+          </Link>
+        </div>
     </div>
   )
 }
