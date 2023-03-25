6328e6f514fb79ad206151a2b9a47666d49363cf
Christella De Sousa <desousa.christella@proton.me>
2023-03-25 11:32:40 +0100

    Fix react router

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,17 @@
 import Banner from '../components/Banner.js'
+import { Link } from 'react-router-dom'
 
 function Home() {
-  return <Banner />
+  return (
+    <div className="header">
+      <Banner />
+      <nav>
+        <Link to="/">Accueil</Link>
+        <Link to="/housing">Housing</Link>
+        <Link to="/about">About</Link>
+      </nav>
+    </div>
+  )
 }
 
 export default Home
