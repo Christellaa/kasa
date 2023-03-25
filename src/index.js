16297fbaef6db43a88509d8dab1ebc92dbfb84db
Christella De Sousa <desousa.christella@proton.me>
2023-03-25 15:00:45 +0100

    Delete duplicate

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './styles/style.scss'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import './styles/index.css'
 import Home from './pages/Home.jsx'
 import Housing from './pages/Housing.jsx'
 import About from './pages/About.jsx'
@@ -24,28 +22,6 @@ const router = createBrowserRouter([
     element: <About />,
   },
 ])
-import Housing from './pages/Housing.jsx'
-import About from './pages/About.jsx'
-import Error from './pages/Error.jsx'
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Home />,
-    errorElement: <Error />,
-  },
-  {
-    path: '/housing',
-    element: <Housing />,
-  },
-  {
-    path: '/about',
-    element: <About />,
-  },
-])
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
 ReactDOM.createRoot(document.getElementById('root')).render(
   <RouterProvider router={router} />
 )
