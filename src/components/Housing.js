95f4d0e8bd1de37a710e9638fd920ea6a51b3d44
Christella De Sousa <desousa.christella@proton.me>
2023-03-27 12:39:47 +0200

    Delete useless divs

--- src/components/Housing.js
+++ src/components/Housing.js
@@ -2,22 +2,18 @@ function Housing({ title, location, tags, host, rating }) {
   return (
     <div>
       <div>
-        <div>
-          <div>
-            <h3>{title}</h3>
-            <p>{location}</p>
-            {tags.map((tag) => (
-              <ul>
-                <li>{tag}</li>
-              </ul>
-            ))}
-          </div>
-          <div>
-            <p>{host.name}</p>
-            <img src={host.picture} alt={host.name} />
-            <p>stars</p>
-          </div>
-        </div>
+        <h3>{title}</h3>
+        <p>{location}</p>
+        {tags.map((tag) => (
+          <ul>
+            <li>{tag}</li>
+          </ul>
+        ))}
+      </div>
+      <div>
+        <p>{host.name}</p>
+        <img src={host.picture} alt={host.name} />
+        <p>stars</p>
       </div>
     </div>
   )
