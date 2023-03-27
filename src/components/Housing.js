f4976dc1c0e9f5d2caddb7c3be24ef823ef205c8
Christella De Sousa <desousa.christella@proton.me>
2023-03-27 22:18:57 +0200

    Add classes

--- src/components/Housing.js
+++ src/components/Housing.js
@@ -2,20 +2,22 @@ function Housing({ title, location, tags, host, rating }) {
   const stars = [1, 2, 3, 4, 5]
 
   return (
-    <div>
-      <div>
-        <h3>{title}</h3>
-        <p>{location}</p>
-        {tags.map((tag) => (
-          <ul>
-            <li>{tag}</li>
-          </ul>
-        ))}
+    <div className="housing__info">
+      <div className="housing__desc">
+        <h3 className="size-tertiary font-primary">{title}</h3>
+        <p className="size-primary">{location}</p>
+        <ul className="housing__tags tag--size">
+          {tags.map((tag) => (
+            <li className="housing__tag">{tag}</li>
+          ))}
+        </ul>
       </div>
-      <div>
-        <p>{host.name}</p>
-        <img src={host.picture} alt={host.name} />
-        <div>
+      <div className="housing__host">
+        <div className="flex-row">
+          <p className="size-primary">{host.name}</p>
+          <img src={host.picture} alt={host.name} />
+        </div>
+        <div className="stars">
           {stars.map((star) =>
             rating >= star ? (
               <i class="fullStar fa-solid fa-star"></i>
