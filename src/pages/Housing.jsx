483c25c2c4cabb53c70f63d306deff1fee2a1b6a
Christella De Sousa <desousa.christella@proton.me>
2023-03-27 11:11:33 +0200

    Use useParams to go to a page by an housing's id

--- src/pages/Housing.jsx
+++ src/pages/Housing.jsx
@@ -1,4 +1,11 @@
-function Housing() {
+import { useParams } from 'react-router-dom'
+import housings from '../data/housings.json'
+function Housings() {
+  const params = useParams()
+  const housing = housings.find((housing) => housing.id === params.id)
+  if (housing === undefined) {
+    return <Error />
+  }
   return (
     <div>
       <h1>Housings</h1>
