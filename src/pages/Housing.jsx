f128923eda8ad521182879ff25c09ac7a4f83c7e
Christella De Sousa <desousa.christella@proton.me>
2023-03-27 11:12:40 +0200

    Create elements equel to housings' one

--- src/pages/Housing.jsx
+++ src/pages/Housing.jsx
@@ -6,6 +6,17 @@ function Housings() {
   if (housing === undefined) {
     return <Error />
   }
+
+  const id = housing.id
+  const title = housing.title
+  //const pictures = housing.pictures
+  //const description = housing.description
+  const host = housing.host
+  const rating = housing.rating
+  const location = housing.location
+  //const equipments = housing.equipments
+  const tags = housing.tags
+
   return (
     <div>
       <h1>Housings</h1>
