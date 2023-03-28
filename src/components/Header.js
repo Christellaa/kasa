4c297f8d0633560d16fcd7854d6073126088f92c
Christella De Sousa <desousa.christella@proton.me>
2023-03-28 19:53:32 +0200

    Add underline to the active link of nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,33 @@
 import logo from '../assets/logo.png'
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
+import { useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
 
 function Header() {
+  const [currentPath, setCurrentPath] = useState('/')
+  const location = useLocation()
+
+  useEffect(() => {
+    setCurrentPath(location.pathname)
+  }, [location])
+
   return (
     <header>
       <img src={logo} alt="Logo de Kasa" className="logo" />
       <nav>
-        <Link to="/" className="links">
+        <Link
+          to="/"
+          className={`${currentPath === '/' ? 'links currentLink' : 'links'}`}
+        >
           Accueil
         </Link>
-        <Link to="/about" className="links">
+        <Link
+          to="/about"
+          className={`${
+            currentPath === '/about' ? 'links currentLink' : 'links'
+          }`}
+        >
           À Propos
         </Link>
       </nav>
