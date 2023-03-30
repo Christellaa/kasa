6c7aee5c3aeb21d462769a1c5d35262a05adf6e0
Christella De Sousa <desousa.christella@proton.me>
2023-03-30 14:43:08 +0200

    Import navbar component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,36 +1,11 @@
 import logo from '../assets/logo.png'
-import { Link } from 'react-router-dom'
-import { useState } from 'react'
-import { useEffect } from 'react'
-import { useLocation } from 'react-router-dom'
+import Navbar from '../components/Navbar'
 
 function Header() {
-  const [currentPath, setCurrentPath] = useState('/')
-  const location = useLocation()
-
-  useEffect(() => {
-    setCurrentPath(location.pathname)
-  }, [location])
-
   return (
     <header>
       <img src={logo} alt="Logo de Kasa" className="logo" />
-      <nav>
-        <Link
-          to="/"
-          className={`${currentPath === '/' ? 'links currentLink' : 'links'}`}
-        >
-          Accueil
-        </Link>
-        <Link
-          to="/about"
-          className={`${
-            currentPath === '/about' ? 'links currentLink' : 'links'
-          }`}
-        >
-          À Propos
-        </Link>
-      </nav>
+      <Navbar />
     </header>
   )
 }
