38cc5c5655b606967197c33f0976ff03c4003535
Christella De Sousa <desousa.christella@proton.me>
2023-04-07 14:14:29 +0200

    Rename Housing to Housings

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './styles/style.scss'
 import Home from './pages/Home.jsx'
-import Housing from './pages/Housing.jsx'
+import Housings from './pages/Housings.jsx'
 import About from './pages/About.jsx'
 import Error from './pages/Error.jsx'
 
@@ -15,7 +15,7 @@ const router = createBrowserRouter([
   },
   {
     path: '/housing/:id',
-    element: <Housing />,
+    element: <Housings />,
   },
   {
     path: '/about',
